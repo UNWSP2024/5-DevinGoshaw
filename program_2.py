90eb5eeb283a90313778ababa0b8787021a22dab
DevinGoshaw <DRGoshaw@students.unwsp.edu>
2025-10-01 08:44:27 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -11,3 +11,27 @@
 # If the answer is correct, a message of congratulations should be displayed.  
 # If the answer is incorrect a message showing the correct answer should be displayed.  
 # The program must use a function that accomplishes part of the needed tasks.
+
+#Name: Devin Goshaw
+#date: 10/1/2025
+#programm: addition math quiz
+
+import random
+
+def check_answer(num1, num2, solution):
+    answer=num1+num2
+    if answer==solution:
+        print('nice job you got the answer correct')
+    else:
+        print('you go the answer wrong the right answer is:', answer)
+
+def quiz():
+    num1=random.randint(0,99999)
+    num2=random.randint(0,99999)
+    print(num1)
+    print('+', num2)
+    print('-----')
+    solution=float(input('answer: '))
+    check_answer(num1, num2, solution)
+        
+quiz()
